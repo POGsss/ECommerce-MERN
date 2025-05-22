43feb0d85e01eba0cdb54d17a27ec8bf836d6959
POGsss <frankiepinat14@gmail.com>
2025-05-23 00:47:44 +0800

    Added Latest Products

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,10 +1,16 @@
 import { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext.jsx';
+import { useEffect, useState } from 'react';
+import ProductItem from './ProductItem.jsx';
 import Title from './Title.jsx';
 
 const LatestCollection = () => {
     const { products } = useContext(ShopContext);
-    console.log(products);
+    const [latestProducts, setLatestProducts] = useState([]);
+
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, []);
 
     return (
         <div className='my-10'>
@@ -13,6 +19,13 @@ const LatestCollection = () => {
                 <p className='w-2/3 m-auto text-xs sm:text-sm md:text-base text-gray-500'>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quis minus, optio illum blanditiis sunt.
                 </p>
+            </div>
+
+            {/* Rendering Products */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-6'>
+                {latestProducts.map((item, index) => (
+                    <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+                ))}
             </div>
         </div>
     )
---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { ShopContext } from "../context/ShopContext"
+
+
+const ProductItem = ({id, image, name, price}) => {
+    const {currency} = useContext(ShopContext)
+
+    return (
+        <Link className="text-gray-800 cursor-pointer" to={`/product/${id}`}>
+            <div className="overflow-hidden">
+                <img className="hover:scale-110 transition ease-in-out" src={image[0]} alt="" />
+            </div>
+            <p className="pt-3 pb-1 text-sm">{name}</p>
+            <p className="text-sm font-subtitle">{currency}{price}</p>
+        </Link>
+    )
+}
+
+export default ProductItem
