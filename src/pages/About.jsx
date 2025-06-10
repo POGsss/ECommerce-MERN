78c7aebb5cc3393e81be69ad6088307d8c02d8eb
POGsss <frankiepinat14@gmail.com>
2025-06-10 23:13:13 +0800

    Added Newsletter To About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { assets } from "../assets/assets"
+import NewsLetterBox from "../components/NewsLetterBox"
 import Title from "../components/Title"
 
 const About = () => {
@@ -7,7 +8,6 @@ const About = () => {
       <div className="text-2xl text-center">
         <Title text1={"ABOUT"} text2={"US"} />
       </div>
-
       <div className="m-10 flex flex-col items-center md:flex-row gap-16">
         <img className="w-full sm:max-w-full md:max-w-[450px]" src={assets.about_img} alt="" />
         <div className="flex flex-col justify-center gap-6 md:w-2/4">
@@ -22,21 +22,22 @@ const About = () => {
       <div className="text-2xl py-4">
         <Title text1={"WHY"} text2={"CHOOSE US"} />
       </div>
-
       <div className="flex flex-col md:flex-row text-sm mb-20">
         <div className="border border-black px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Quality Assurance:</b>
-          <p>We meticulously select and classified each product to ensure it meets our stringent quality standards.</p>
+          <p className="text-gray-500">We meticulously select and classified each product to ensure it meets our stringent quality standards.</p>
         </div>
         <div className="border border-black px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Convinience:</b>
-          <p>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+          <p className="text-gray-500">With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
         </div>
         <div className="border border-black px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
           <b>Customer Service:</b>
-          <p>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is priority.</p>
+          <p className="text-gray-500">Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is priority.</p>
         </div>
       </div>
+
+      <NewsLetterBox />
     </div>
   )
 }
