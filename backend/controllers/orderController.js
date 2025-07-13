21986c2c3313b46a999732fe16c9c89e27993901
POGsss <frankiepinat14@gmail.com>
2025-07-13 16:57:55 +0800

    Configure Stripe Payment Gateway

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+// Global Variables
+const currency = "php";
+const deliveryFee = 30;
+
+// Stripe Initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place Order Functionality COD
 const placeOrderCOD = async (req, res) => {
@@ -36,7 +44,63 @@ const placeOrderCOD = async (req, res) => {
 
 // Place Order Functionality Stripe
 const placeOrderStripe = async (req, res) => {
+    try {
+        // Getting User Data
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers;
 
+        // Creating Order Data
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        };
+
+        // Creating Order
+        const newOrder = await orderModel.create(orderData);
+        await newOrder.save();
+
+        // Creating Line Items
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }));
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: "Delivery Fee"
+                },
+                unit_amount: deliveryFee * 100
+            },
+            quantity: item.quantity
+        });
+
+        // Creating Session
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: "payment",
+        });
+
+        // Sending Response
+        res.json({success: true, session_url: session_url});
+    } catch (error) {
+        // Logging Error
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
 }
 
 // Place Order Functionality Razorpay
